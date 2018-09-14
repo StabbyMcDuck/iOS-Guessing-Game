6a40ebec99f19cd92e896af53dd00e544db6143a
Imhoff, Regina L <imhoffr@usbrngspri204dd.local>
2018-09-14 11:19:20 -0500

    Initial Commit

---
+++ myGuessingGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myGuessingGame
+//
+//  Created by Imhoff, Regina L. on 9/14/18.
+//  Copyright © 2018 Regina Imhoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
