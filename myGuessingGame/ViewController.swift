06fe25070f210afd780f85d361343c3eaccedf52
Regina Imhoff <regina.imhoff@merck.com>
2018-09-15 09:55:51 -0500

    end of video 3

--- myGuessingGame/ViewController.swift
+++ myGuessingGame/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var guessField: UITextField!
+    
+    @IBOutlet var guessLabel: UILabel!
+    
+    @IBOutlet var gamesLabel: UILabel!
+    
+    @IBOutlet var scoreLabel: UILabel!
+    
+    @IBOutlet var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func newGameBtn(_ sender: Any) {
+    }
+    
+    @IBAction func checkAnswerBtn(_ sender: Any) {
+    }
+    
 }
 
